52d9d6512e22d5b50564982b17168d8d66460e99
pbronx <pbronx@gmail.com>
2024-07-09 21:25:38 +0200

    Update update-data.js

--- netlify/var2/update-data.js
+++ netlify/var2/update-data.js
@@ -1,1 +1,20 @@
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Method Not Allowed',
+    };
+  }
+
+  const data = JSON.parse(event.body).data;
+
+  // Memorizzare il dato in un file o database, qui usiamo un file per semplicità
+  const fs = require('fs');
+  fs.writeFileSync('/tmp/data.txt', data);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Data received successfully' }),
+  };
+};
 
