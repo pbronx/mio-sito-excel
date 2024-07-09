94bf614d3939c5edeffede0c4d9c87dee2951862
pbronx <pbronx@gmail.com>
2024-07-09 16:04:53 +0200

    Update get-valore.js

--- netlify/functions/get-valore.js
+++ netlify/functions/get-valore.js
@@ -1,1 +1,15 @@
-
+exports.handler = async function(event, context) {
+    const fs = require('fs').promises;
+    try {
+        const data = await fs.readFile('/tmp/valore.txt', 'utf8');
+        return {
+            statusCode: 200,
+            body: data
+        };
+    } catch (error) {
+        return {
+            statusCode: 200,
+            body: "Nessun aggiornamento disponibile"
+        };
+    }
+};
