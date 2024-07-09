a99e01b247602e747f3b2d9f249efe218ec1a653
pbronx <pbronx@gmail.com>
2024-07-09 18:21:21 +0200

    Update get-valore.js

--- netlify/functions/get-valore.js
+++ netlify/functions/get-valore.js
@@ -1,9 +1,29 @@
+const faunadb = require('faunadb');
+const q = faunadb.query;
+
+const client = new faunadb.Client({
+  secret: process.env.FAUNA_SECRET_KEY
+});
+
 exports.handler = async function(event, context) {
+  try {
+    const result = await client.query(
+      q.Get(q.Match(q.Index("valore_by_id"), "ultimo_valore (pda)"))
+    );
     return {
-        statusCode: 200,
-        body: "Nessun aggiornamento disponibile perbacco",
-        headers: {
-            'Content-Type': 'text/plain'
-        }
+      statusCode: 200,
+      body: result.data.contenuto,
+      headers: {
+        'Content-Type': 'text/plain'
+      }
     };
+  } catch (error) {
+    return {
+      statusCode: 200,
+      body: "Nessun aggiornamento disponibile (pda)",
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    };
+  }
 };
