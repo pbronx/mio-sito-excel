f8ff3d0cd0615605a46b1a28aa6001846d0ef619
pbronx <pbronx@gmail.com>
2024-07-09 21:26:13 +0200

    Update get-data.js

--- netlify/var2/get-data.js
+++ netlify/var2/get-data.js
@@ -1,1 +1,15 @@
+exports.handler = async (event, context) => {
+  const fs = require('fs');
+  let data = '';
 
+  try {
+    data = fs.readFileSync('/tmp/data.txt', 'utf8');
+  } catch (err) {
+    data = 'No data available';
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data }),
+  };
+};
